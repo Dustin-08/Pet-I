763a6b813af185e24538b499af41e5d251d8baf2
Dustin-08 <cjhjik1@gmail.com>
2024-05-25 21:28:56 +0900

    Update: 리튬 폴리머 배터리 전압 및 잔략량 측정 코드 추가

--- HW/Not_CSV/Pet-I_Final/Pet-I_Final.ino
+++ HW/Not_CSV/Pet-I_Final/Pet-I_Final.ino
@@ -5,6 +5,7 @@ Use Sensor: Arduino Pro Micro, MLX-90614, HC-06, SZH-, MPU 9250, Li-ion_Battery,
 */
 
 // ToDo: SD 카드 복구 기능 추가, Li-ion Battery
+// ToDo: 리튬폴리머 배터리의 전압 및 잔량을 측정하면서 동시에 작동 시키는 방법은?
 
 // 0. Libraries
 
@@ -72,6 +73,19 @@ const int PulseWire = 0;
 const int LED13 = 13;
 int Threshold = 550;
 
+// Voltage and Battery
+// 아날로그 핀 정의
+const int batteryPin = A1;
+
+// 전압 레퍼런스 값 (아두이노는 5V를 기준으로 함)
+const float referenceVoltage = 5.0;
+
+// 아날로그 읽기 최대 값 (아두이노의 10비트 ADC는 0-1023 값을 가짐)
+const int maxADCValue = 1023;
+
+// 배터리 전압 최대 값 (예: 12V 배터리 사용 시)
+const float maxBatteryVoltage = 5.0;
+
 // 3. void setup()
 void setup()
 {
@@ -80,7 +94,7 @@ void setup()
     BT_Init();
     MLX_Init();
     SZH_Init();
-    Battery_Init();
+    //Battery_Init();
     SdCard_Init();
 }
 
@@ -114,10 +128,6 @@ void SZH_Init(){
     if (pulseSensor.begin()) {
         Serial.println("We created a pulseSensor Object !");
     }
-}
-
-void Battery_Init(){
-    // test
 }
 
 void SdCard_Init(){
@@ -254,7 +264,26 @@ void SZH_Loop(){
 }
 
 void Battery_Loop(){
-    //test
+    // 배터리 핀에서 아날로그 값 읽기
+    int analogValue = analogRead(batteryPin);
+  
+    // 읽은 값을 실제 전압으로 변환
+    float batteryVoltage = (analogValue * referenceVoltage) / maxADCValue;
+  
+    // 배터리 전압 비율 계산 (0-1)
+    float batteryPercentage = (batteryVoltage / maxBatteryVoltage) * 100;
+  
+    // 배터리 전압과 잔량을 시리얼 모니터에 출력
+    Serial.print("Battery Voltage: ");
+    Serial.print(batteryVoltage);
+    Serial.println(" V");
+  
+    Serial.print("Battery Percentage: ");
+    Serial.print(batteryPercentage);
+    Serial.println(" %");
+  
+    // 1분 대기
+    delay(60000);
 }
 
 void SdCard_Loop(){
