4ed1d497cec33e21a5a1dfdfde01dbee3b3b1648
Dustin-08 <cjhjik1@gmail.com>
2024-06-04 10:33:19 +0900

    Update: MPU 코드 수정중

--- HW/Not_CSV/Pet-I_Final_v2/Pet-I_Final_v2.ino
+++ HW/Not_CSV/Pet-I_Final_v2/Pet-I_Final_v2.ino
@@ -67,18 +67,21 @@ void setup()
     BT_Init();
     MLX_Init();
     SZH_Init();
-    Battery_Init();
+    //Battery_Init();
     SdCard_Init();
 }
 
 // 4. setup() 모음
 void MPU_Init(){
     Wire.begin();
-    if (!mpu.setup(0x68)) {
-      Serial.println("MPU 연결 실패!");
-      while (1);
-    }
+    mpu.initialize(); // Correct method to initialize MPU9250
+//    if (!mpu.testConnection()) {
+//        Serial.println("MPU 연결 실패!");
+//        while (1);
+//    }
+    Serial.println("MPU 연결 성공!");
 }
+
 
 void BT_Init(){
     BT.begin(9600); // BT를 보드레이트 9600으로 설정
@@ -117,46 +120,38 @@ void SdCard_Loop();
 
 // 6. function-----------------------------------------------------------------------------------------
 void MPU_Loop(){
-    getAccel_Data();
-    getGyro_Data();
-    getCompassDate_calibrated(); 
-    getHeading();               
-    getTiltHeading();
-
-    // Serial.println("calibration parameter: ");
-    // Serial.print(mx_centre);
-    // Serial.print("         ");
-    // Serial.print(my_centre);
-    // Serial.print("         ");
-    // Serial.println(mz_centre);
-    // Serial.println("     ");
-
-
-    Serial.println("Acceleration(g) of X,Y,Z:");
-    Serial.print(Axyz[0]);
-    Serial.print(",");
-    Serial.print(Axyz[1]);
-    Serial.print(",");
-    Serial.println(Axyz[2]);
-    Serial.println("Gyro(degress/s) of X,Y,Z:");
-    Serial.print(Gxyz[0]);
-    Serial.print(",");
-    Serial.print(Gxyz[1]);
-    Serial.print(",");
-    Serial.println(Gxyz[2]);
-    // Serial.println("Compass Value of X,Y,Z:");
-    // Serial.print(Mxyz[0]);
-    // Serial.print(",");
-    // Serial.print(Mxyz[1]);
-    // Serial.print(",");
-    // Serial.println(Mxyz[2]);
-    // Serial.println("The clockwise angle between the magnetic north and X-Axis:");
-    // Serial.print(heading);
-    // Serial.println(" ");
-    // Serial.println("The clockwise angle between the magnetic north and the projection of the positive X-Axis in the horizontal plane:");
-    // Serial.println(tiltheading);
-    // Serial.println("   ");
-    // Serial.println();
+    // 센서 데이터 업데이트
+    mpu.update();
+  
+    // 가속도 데이터
+    float accelX = mpu.getAccX();
+    float accelY = mpu.getAccY();
+    float accelZ = mpu.getAccZ();
+  
+    // 자이로 데이터
+    float gyroX = mpu.getGyroX();
+    float gyroY = mpu.getGyroY();
+    float gyroZ = mpu.getGyroZ();
+  
+    // 가속도 크기 계산
+    float accelMagnitude = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
+    // 자이로 크기 계산
+    float gyroMagnitude = sqrt(gyroX * gyroX + gyroY * gyroY + gyroZ * gyroZ);
+  
+    // 강아지 상태 판단
+    String dogState;
+    if (accelMagnitude < 0.5 && gyroMagnitude < 0.5) {
+      dogState = "가만히 있음";
+    } else if (accelMagnitude < 1.5 && gyroMagnitude < 1.5) {
+      dogState = "걷는 중";
+    } else if (accelMagnitude < 3.0 && gyroMagnitude < 3.0) {
+      dogState = "뛰는 중";
+    } else {
+      dogState = "몸을 터는 중";
+    }
+  
+    // 상태 출력
+    Serial.println("강아지 상태: " + dogState);
     delay(60000);
 }
 
@@ -172,19 +167,6 @@ void BT_Loop(){
             //BT.println("Pet-I Turn On");
             // delay(1000);
             MPU_Loop();
-            BT.println("Acceleration(g) of X,Y,Z:");
-            BT.print(Axyz[0]);
-            BT.print(",");
-            BT.print(Axyz[1]);
-            BT.print(",");
-            BT.println(Axyz[2]);
-            BT.println("Gyro(degress/s) of X,Y,Z:");
-            BT.print(Gxyz[0]);
-            BT.print(",");
-            BT.print(Gxyz[1]);
-            BT.print(",");
-            BT.println(Gxyz[2]);
-            //BT.print("Acceleration(g) of X,Y,Z: " + Axyz[0] + ", ")
             MLX_Loop();
             SZH_Loop();
             Battery_Loop();
@@ -261,128 +243,6 @@ void Battery_Loop(){
 
 void SdCard_Loop(){
     //test
-}
-
-void getHeading(void)
-{
-    heading = 180 * atan2(Mxyz[1], Mxyz[0]) / PI;
-    if (heading < 0) heading += 360;
-}
-
-void getTiltHeading(void)
-{
-    float pitch = asin(-Axyz[0]);
-    float roll = asin(Axyz[1] / cos(pitch));
-
-    float xh = Mxyz[0] * cos(pitch) + Mxyz[2] * sin(pitch);
-    float yh = Mxyz[0] * sin(roll) * sin(pitch) + Mxyz[1] * cos(roll) - Mxyz[2] * sin(roll) * cos(pitch);
-    float zh = -Mxyz[0] * cos(roll) * sin(pitch) + Mxyz[1] * sin(roll) + Mxyz[2] * cos(roll) * cos(pitch);
-    tiltheading = 180 * atan2(yh, xh) / PI;
-    if (yh < 0)    tiltheading += 360;
-}
-
-void Mxyz_init_calibrated ()
-{
-
-    Serial.println(F("Before using 9DOF,we need to calibrate the compass frist,It will takes about 2 minutes."));
-    Serial.print("  ");
-    Serial.println(F("During  calibratting ,you should rotate and turn the 9DOF all the time within 2 minutes."));
-    Serial.print("  ");
-    Serial.println(F("If you are ready ,please sent a command data 'ready' to start sample and calibrate."));
-    while (!Serial.find("ready"));
-    Serial.println("  ");
-    Serial.println("ready");
-    Serial.println("Sample starting......");
-    Serial.println("waiting ......");
-
-    get_calibration_Data ();
-
-    Serial.println("     ");
-    Serial.println("compass calibration parameter ");
-    Serial.print(mx_centre);
-    Serial.print("     ");
-    Serial.print(my_centre);
-    Serial.print("     ");
-    Serial.println(mz_centre);
-    Serial.println("    ");
-}
-
-
-void get_calibration_Data ()
-{
-    for (int i = 0; i < sample_num_mdate; i++)
-    {
-        get_one_sample_date_mxyz();
-
-        if (mx_sample[2] >= mx_sample[1])mx_sample[1] = mx_sample[2];
-        if (my_sample[2] >= my_sample[1])my_sample[1] = my_sample[2]; //find max value
-        if (mz_sample[2] >= mz_sample[1])mz_sample[1] = mz_sample[2];
-
-        if (mx_sample[2] <= mx_sample[0])mx_sample[0] = mx_sample[2];
-        if (my_sample[2] <= my_sample[0])my_sample[0] = my_sample[2]; //find min value
-        if (mz_sample[2] <= mz_sample[0])mz_sample[0] = mz_sample[2];
-
-    }
-
-    mx_max = mx_sample[1];
-    my_max = my_sample[1];
-    mz_max = mz_sample[1];
-
-    mx_min = mx_sample[0];
-    my_min = my_sample[0];
-    mz_min = mz_sample[0];
-
-    mx_centre = (mx_max + mx_min) / 2;
-    my_centre = (my_max + my_min) / 2;
-    mz_centre = (mz_max + mz_min) / 2;
-}
-
-void get_one_sample_date_mxyz()
-{
-    getCompass_Data();
-    mx_sample[2] = Mxyz[0];
-    my_sample[2] = Mxyz[1];
-    mz_sample[2] = Mxyz[2];
-}
-
-void getAccel_Data(void)
-{
-    accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-    Axyz[0] = (double) ax / 16384;
-    Axyz[1] = (double) ay / 16384;
-    Axyz[2] = (double) az / 16384;
-}
-
-void getGyro_Data(void)
-{
-    accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-
-    Gxyz[0] = (double) gx * 250 / 32768;
-    Gxyz[1] = (double) gy * 250 / 32768;
-    Gxyz[2] = (double) gz * 250 / 32768;
-}
-
-void getCompass_Data(void)
-{
-    I2C_M.writeByte(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01); //enable the magnetometer
-    delay(10);
-    I2C_M.readBytes(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L, 6, buffer_m);
-
-    mx = ((int16_t)(buffer_m[1]) << 8) | buffer_m[0] ;
-    my = ((int16_t)(buffer_m[3]) << 8) | buffer_m[2] ;
-    mz = ((int16_t)(buffer_m[5]) << 8) | buffer_m[4] ;
-
-    Mxyz[0] = (double) mx * 1200 / 4096;
-    Mxyz[1] = (double) my * 1200 / 4096;
-    Mxyz[2] = (double) mz * 1200 / 4096;
-}
-
-void getCompassDate_calibrated ()
-{
-    getCompass_Data();
-    Mxyz[0] = Mxyz[0] - mx_centre;
-    Mxyz[1] = Mxyz[1] - my_centre;
-    Mxyz[2] = Mxyz[2] - mz_centre;
 }
 
 // 7. void loop()----------------------------------------------------------------------------
