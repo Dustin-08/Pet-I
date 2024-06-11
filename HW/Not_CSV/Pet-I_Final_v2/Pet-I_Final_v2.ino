0dcda84f18a576b6b3e3b56ab67948cd87b2b521
Dustin-08 <cjhjik1@gmail.com>
2024-06-12 07:34:22 +0900

    Update: RGB 코드 추가, sd 코드 추가중

--- HW/Not_CSV/Pet-I_Final_v2/Pet-I_Final_v2.ino
+++ HW/Not_CSV/Pet-I_Final_v2/Pet-I_Final_v2.ino
@@ -4,15 +4,17 @@ Developed by Dustin Choi.
 Used Sensor: Arduino Pro Mini, MLX-90614, HC-06, SZH-, MPU 9250, Li-ion_Battery, Charging Module, RGB Led, SD Card Module
 */
 
-// ToDo: SD 카드 복구 기능 추가, Li-ion Battery
-// ToDo: 리튬폴리머 배터리의 전압 및 잔량을 측정하면서 동시에 작동 시키는 방법은?
-// ToDo: 심박은 delay(60000)으로, 온도와 배터리는 delay(600000)으로
+// 최종 ToDo: SD 카드, (RGB 코드 추가), 자이로로 state 파악
+
+// ToDo: SD 카드 복구 기능 추가
+// ToDo: 리튬폴리머 배터리의 전압 및 잔량을 측정하면서 동시에 작동 시키는 방법은? -> A1 핀을 VCC 라인에 연결
+// ToDo: 심박은 delay(60000)으로, 온도와 배터리는 delay(600000)으로 -> 현재 이와 같이 동작
 // ToDo: 자이로로 state 파악
-// ToDo: 앱에서 id값 받으면 블루투스 기기명 변경
+// ToDo: 앱에서 id값 받으면 블루투스 기기명 변경 -> AT 모드 사용해야함
 // ToDo: 앱에서 RGB int 값 받으면 RGB Led에 값 적용
 // ToDo: Led 페어링 상태 표시
-// ToDo: 강아지 나이별로 주기 설정 후 적용
-// ToDo: 케이스에 회로도 적용
+// ToDo: 강아지 나이별로 주기 설정 후 적용 -> 추후 집중학기제 때 도입 예정
+// ToDo: 케이스에 회로도 적용 -> 제작 완료
 
 // 0. Libraries
 
@@ -26,7 +28,41 @@ Used Sensor: Arduino Pro Mini, MLX-90614, HC-06, SZH-, MPU 9250, Li-ion_Battery,
 
 // 1. 변수 선언
 // MPU--------------------------------------------------------------------------
-MPU9250 mpu;
+MPU9250 accelgyro;
+I2Cdev   I2C_M;
+
+uint8_t buffer_m[6];
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int16_t   mx, my, mz;
+
+float heading;
+float tiltheading;
+float Axyz[3];
+float Gxyz[3];
+float Mxyz[3];
+#define sample_num_mdate  5000
+volatile float mx_sample[3];
+volatile float my_sample[3];
+volatile float mz_sample[3];
+
+static float mx_centre = 0;
+static float my_centre = 0;
+static float mz_centre = 0;
+
+volatile int mx_max = 0;
+volatile int my_max = 0;
+volatile int mz_max = 0;
+
+volatile int mx_min = 0;
+volatile int my_min = 0;
+volatile int mz_min = 0;
+
+float temperature;
+float pressure;
+float atm;
+float altitude;
 
 // BT----------------------------------------------------------------------------
 #define BTtx       8 // 블루투스 모듈의 tx를 D8으로 설정
@@ -34,6 +70,12 @@ MPU9250 mpu;
 
 SoftwareSerial BT(BTtx, BTrx); // tx, rx
 char data = 0; // 앱을 통해 0 또는 1이라는 문자열을 받을건데 0이 꺼지는 default 값이므로 0으로 설정
+
+// 주기적 데이터 전송을 위한 타이머 변수
+unsigned long previousMillisHeartRate = 0;
+unsigned long previousMillisTempBattery = 0;
+const long intervalHeartRate = 60000; // 1분
+const long intervalTempBattery = 600000; // 10분
 
 // MLX-90614---------------------------------------------------------------------
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
@@ -46,7 +88,7 @@ const int PulseWire = 0;
 const int LED13 = 13;
 int Threshold = 550;
 
-// Voltage and Battery
+// Voltage and Battery--------------------------------------------------------------
 // 아날로그 핀 정의
 const int batteryPin = A1;
 
@@ -59,11 +101,15 @@ const int maxADCValue = 1023;
 // 배터리 전압 최대 값 (5V 배터리 사용 시)
 const float maxBatteryVoltage = 5.0;
 
-// 주기적 데이터 전송을 위한 타이머 변수
-unsigned long previousMillisHeartRate = 0;
-unsigned long previousMillisTempBattery = 0;
-const long intervalHeartRate = 60000; // 1분
-const long intervalTempBattery = 600000; // 10분
+// SD Card---------------------------------------------------------------------------
+const int chipSelect = 10; // Arduino Pro Mini의 CS는 D10
+SdFat sd;
+SdFile myFile;
+
+// RGB Led---------------------------------------------------------------------------
+int red = 9;
+int green = 7;
+int blue = 8;
 
 // 3. void setup()
 void setup()
@@ -73,19 +119,18 @@ void setup()
     BT_Init();
     MLX_Init();
     SZH_Init();
-    //Battery_Init();
+    //Battery_Init(); // setup() 부분 존재 x
     SdCard_Init();
+    RGB_Init();
 }
 
 // 4. setup() 모음
 void MPU_Init(){
     Wire.begin();
-    mpu.initialize(); // Correct method to initialize MPU9250
-//    if (!mpu.testConnection()) {
-//        Serial.println("MPU 연결 실패!");
-//        while (1);
-//    }
-    Serial.println("MPU 연결 성공!");
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU9250 connection successful" : "MPU9250 connection failed");
 }
 
 void BT_Init(){
@@ -115,6 +160,12 @@ void SdCard_Init(){
     // test
 }
 
+void RGB_Init(){
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
 // 5. function 선언
 void MPU_Loop();
 void BT_Loop();
@@ -122,39 +173,51 @@ void MLX_Loop();
 void SZH_Loop();
 void Battery_Loop();
 void SdCard_Loop();
+void RGB_Loop();
+void blinkRGB_Loop();
 void sendDataOverBluetooth();
 
 // 6. function-----------------------------------------------------------------------------------------
 void MPU_Loop(){
-    // 센서 데이터 업데이트
-    mpu.update();
-  
-    // 가속도 데이터
-    float accelX = mpu.getAccX();
-    float accelY = mpu.getAccY();
-    float accelZ = mpu.getAccZ();
-  
-    // 자이로 데이터
-    float gyroX = mpu.getGyroX();
-    float gyroY = mpu.getGyroY();
-    float gyroZ = mpu.getGyroZ();
-  
-    // 가속도 크기 계산
-    float accelMagnitude = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
-    // 자이로 크기 계산
-    float gyroMagnitude = sqrt(gyroX * gyroX + gyroY * gyroY + gyroZ * gyroZ);
-  
+    getAccel_Data();
+    getGyro_Data();
+    getCompassDate_calibrated(); 
+    getHeading();               
+    getTiltHeading();
+
+    // Serial.println("calibration parameter: ");
+    // Serial.print(mx_centre);
+    // Serial.print("         ");
+    // Serial.print(my_centre);
+    // Serial.print("         ");
+    // Serial.println(mz_centre);
+    // Serial.println("     ");
+
+
+    Serial.println("Acceleration(g) of X,Y,Z:");
+    Serial.print(Axyz[0]);
+    Serial.print(",");
+    Serial.print(Axyz[1]);
+    Serial.print(",");
+    Serial.println(Axyz[2]);
+    Serial.println("Gyro(degress/s) of X,Y,Z:");
+    Serial.print(Gxyz[0]);
+    Serial.print(",");
+    Serial.print(Gxyz[1]);
+    Serial.print(",");
+    Serial.println(Gxyz[2]);
+    //delay(60000);
     // 강아지 상태 판단
     String dogState;
-    if (accelMagnitude < 0.5 && gyroMagnitude < 0.5) {
-      dogState = "가만히 있음";
-    } else if (accelMagnitude < 1.5 && gyroMagnitude < 1.5) {
-      dogState = "걷는 중";
-    } else if (accelMagnitude < 3.0 && gyroMagnitude < 3.0) {
-      dogState = "뛰는 중";
-    } else {
-      dogState = "몸을 터는 중";
-    }
+//    if (accelMagnitude < 0.5 && gyroMagnitude < 0.5) {
+//      dogState = "가만히 있음";
+//    } else if (accelMagnitude < 1.5 && gyroMagnitude < 1.5) {
+//      dogState = "걷는 중";
+//    } else if (accelMagnitude < 3.0 && gyroMagnitude < 3.0) {
+//      dogState = "뛰는 중";
+//    } else {
+//      dogState = "몸을 터는 중";
+//    }
   
     // 상태 출력
     Serial.println("강아지 상태: " + dogState);
@@ -169,6 +232,7 @@ void BT_Loop(){
         if (data == '1') { // 블루투스 연결 상태 확인
             // 연결된 경우: Pet-I 작동 On
             BT.println("Pet-I Turn On");
+            RGB_Loop();
             
             // 1분마다 심박 전송
             if (currentMillis - previousMillisHeartRate >= intervalHeartRate) {
@@ -185,6 +249,7 @@ void BT_Loop(){
         } else if (data == '0') {
             // 연결되지 않은 경우: Pet-I 작동 OFF
             BT.println("Pet-I Turn Off");
+            blinkRGB_Loop();
             delay(1000);
         }
     }
@@ -244,11 +309,151 @@ void SdCard_Loop(){
     //test
 }
 
+// RGB Led 루프
+void RGB_Loop(){
+   digitalWrite(green, HIGH);
+}
+
+void blinkRGB_Loop(){
+  digitalWrite(green, HIGH);
+  // 0.5초 동안 대기합니다.
+  delay(500);
+  // 3색 LED의 초록색 LED가 꺼지도록 합니다.
+  digitalWrite(green, LOW);
+  // 0.5초 동안 대기합니다.
+  delay(500);
+}
+
+// 블루투스 처리 위한 데이터 타입 정리 함수
 void sendDataOverBluetooth(String dataType, float dataValue) {
     BT.print(dataType);
     BT.print(": ");
     BT.println(dataValue);
 }
+
+// MPU 처리 위한 함수 모음
+void getHeading(void)
+{
+    heading = 180 * atan2(Mxyz[1], Mxyz[0]) / PI;
+    if (heading < 0) heading += 360;
+}
+
+void getTiltHeading(void)
+{
+    float pitch = asin(-Axyz[0]);
+    float roll = asin(Axyz[1] / cos(pitch));
+
+    float xh = Mxyz[0] * cos(pitch) + Mxyz[2] * sin(pitch);
+    float yh = Mxyz[0] * sin(roll) * sin(pitch) + Mxyz[1] * cos(roll) - Mxyz[2] * sin(roll) * cos(pitch);
+    float zh = -Mxyz[0] * cos(roll) * sin(pitch) + Mxyz[1] * sin(roll) + Mxyz[2] * cos(roll) * cos(pitch);
+    tiltheading = 180 * atan2(yh, xh) / PI;
+    if (yh < 0)    tiltheading += 360;
+}
+
+void Mxyz_init_calibrated ()
+{
+
+    Serial.println(F("Before using 9DOF,we need to calibrate the compass frist,It will takes about 2 minutes."));
+    Serial.print("  ");
+    Serial.println(F("During  calibratting ,you should rotate and turn the 9DOF all the time within 2 minutes."));
+    Serial.print("  ");
+    Serial.println(F("If you are ready ,please sent a command data 'ready' to start sample and calibrate."));
+    while (!Serial.find("ready"));
+    Serial.println("  ");
+    Serial.println("ready");
+    Serial.println("Sample starting......");
+    Serial.println("waiting ......");
+
+    get_calibration_Data ();
+
+    Serial.println("     ");
+    Serial.println("compass calibration parameter ");
+    Serial.print(mx_centre);
+    Serial.print("     ");
+    Serial.print(my_centre);
+    Serial.print("     ");
+    Serial.println(mz_centre);
+    Serial.println("    ");
+}
+
+
+void get_calibration_Data ()
+{
+    for (int i = 0; i < sample_num_mdate; i++)
+    {
+        get_one_sample_date_mxyz();
+
+        if (mx_sample[2] >= mx_sample[1])mx_sample[1] = mx_sample[2];
+        if (my_sample[2] >= my_sample[1])my_sample[1] = my_sample[2]; //find max value
+        if (mz_sample[2] >= mz_sample[1])mz_sample[1] = mz_sample[2];
+
+        if (mx_sample[2] <= mx_sample[0])mx_sample[0] = mx_sample[2];
+        if (my_sample[2] <= my_sample[0])my_sample[0] = my_sample[2]; //find min value
+        if (mz_sample[2] <= mz_sample[0])mz_sample[0] = mz_sample[2];
+
+    }
+
+    mx_max = mx_sample[1];
+    my_max = my_sample[1];
+    mz_max = mz_sample[1];
+
+    mx_min = mx_sample[0];
+    my_min = my_sample[0];
+    mz_min = mz_sample[0];
+
+    mx_centre = (mx_max + mx_min) / 2;
+    my_centre = (my_max + my_min) / 2;
+    mz_centre = (mz_max + mz_min) / 2;
+}
+
+void get_one_sample_date_mxyz()
+{
+    getCompass_Data();
+    mx_sample[2] = Mxyz[0];
+    my_sample[2] = Mxyz[1];
+    mz_sample[2] = Mxyz[2];
+}
+
+void getAccel_Data(void)
+{
+    accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+    Axyz[0] = (double) ax / 16384;
+    Axyz[1] = (double) ay / 16384;
+    Axyz[2] = (double) az / 16384;
+}
+
+void getGyro_Data(void)
+{
+    accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+
+    Gxyz[0] = (double) gx * 250 / 32768;
+    Gxyz[1] = (double) gy * 250 / 32768;
+    Gxyz[2] = (double) gz * 250 / 32768;
+}
+
+void getCompass_Data(void)
+{
+    I2C_M.writeByte(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01); //enable the magnetometer
+    delay(10);
+    I2C_M.readBytes(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L, 6, buffer_m);
+
+    mx = ((int16_t)(buffer_m[1]) << 8) | buffer_m[0] ;
+    my = ((int16_t)(buffer_m[3]) << 8) | buffer_m[2] ;
+    mz = ((int16_t)(buffer_m[5]) << 8) | buffer_m[4] ;
+
+    Mxyz[0] = (double) mx * 1200 / 4096;
+    Mxyz[1] = (double) my * 1200 / 4096;
+    Mxyz[2] = (double) mz * 1200 / 4096;
+}
+
+void getCompassDate_calibrated ()
+{
+    getCompass_Data();
+    Mxyz[0] = Mxyz[0] - mx_centre;
+    Mxyz[1] = Mxyz[1] - my_centre;
+    Mxyz[2] = Mxyz[2] - mz_centre;
+}
+
 
 // 7. void loop()----------------------------------------------------------------------------
 void loop()
